27a6168aff1d8ced688a16a29657cc9e53c0d138
Ellen Schrader <mail@ellen-schrader.de>
2025-03-22 12:40:01 +0000

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Ellen Schrader on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ landmarksTests/landmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksTests.swift
+//  landmarksTests
+//
+//  Created by Ellen Schrader on 22/03/2025.
+//
+
+import Testing
+@testable import landmarks
+
+struct landmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
